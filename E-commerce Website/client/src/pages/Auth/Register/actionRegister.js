e2105d250d1b38172944436bb9d17916669bea92
Abu Huraira <162177269+Harry-47@users.noreply.github.com>
2026-09-16 16:08:55 +0500

    Update actionRegister.js

--- E-commerce Website/client/src/pages/Auth/Register/actionRegister.js
+++ E-commerce Website/client/src/pages/Auth/Register/actionRegister.js
@@ -4,18 +4,21 @@ import axiosApi from "../../../utils/axiosInstance";
 
 const actionRegister = async ({ request }) => {
   const formData = await request.formData();
-
   const data = Object.fromEntries(formData);
 
-  const res = await axiosApi.post(`${import.meta.env.VITE_API_URL}auth/register`, data);
+  try {
+    
+    const res = await axiosApi.post('auth/register', data);
 
-  
-
-  if (res.status === 201) {
-    toast.success(res.data?.msg || "Registration successful! Please log in.");
-    return redirect(`/auth/login`);
-  } else {
-    return "Something went wrong" ;
+    if (res.status === 201) {
+      toast.success(res.data?.msg || "Registration successful! Please log in.");
+      return redirect(`/auth/login`);
+    }
+  } catch (err) {
+    const errorMessage = err.response?.data?.error || "Something went wrong";
+    
+    return { error: errorMessage };
   }
 };
+
 export default actionRegister;
