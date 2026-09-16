c1c307d394386bf199ed4980aee34edef63c9b28
Abu Huraira <162177269+Harry-47@users.noreply.github.com>
2026-09-16 15:01:29 +0500

    Update server.js

--- E-commerce Website/server/server.js
+++ E-commerce Website/server/server.js
@@ -32,25 +32,19 @@ app.use(
 
 app.use(passport.initialize());
 
-
-// Middleware
-const allowedOrigins = [
-  'https://e-commerce-website-gilt-delta.vercel.app/',
-  'https://e-commerce-website-git-main-harry-47s-projects.vercel.app/',
-  'https://e-commerce-website-7kbvh8cnu-harry-47s-projects.vercel.app/',
-  'http://localhost:5173', // local testing 
-  'http://localhost:3000'
-];
-
 app.use(cors({
   origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps or curl requests)
     if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
+    
+    // only vercel and local host are allowed to access resources
+    const isVercel = origin.endsWith('.vercel.app');
+    const isLocal = origin === 'http://localhost:5173' || origin === 'http://localhost:3000';
+
+    if (isVercel || isLocal) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('CORS blocked!'), false);
     }
-    return callback(null, true);
   },
   credentials: true
 }));
