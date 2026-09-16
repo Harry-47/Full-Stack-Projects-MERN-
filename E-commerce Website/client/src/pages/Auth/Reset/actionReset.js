d75cef3c862e8ea8550364150828a07935919200
Abu Huraira <162177269+Harry-47@users.noreply.github.com>
2026-09-16 16:12:35 +0500

    Update actionReset.js

--- E-commerce Website/client/src/pages/Auth/Reset/actionReset.js
+++ E-commerce Website/client/src/pages/Auth/Reset/actionReset.js
@@ -3,7 +3,7 @@ import axiosApi from "../../../utils/axiosInstance";
 
  const resetPasswordApi = async (token, newPassword) => {
     
-    const response = await axiosApi.post(`${import.meta.env.VITE_API_URL}/auth/reset-password/${token}`, {newPassword});
+    const response = await axiosApi.post(`${import.meta.env.VITE_API_URL}auth/reset-password/${token}`, {newPassword});
 
     const {data} = response;
 
